d94ac9165dc14e7d7824bdec34f5bd5a252b858a
Sarvendra Vikram <anshusingh639422@gmail.com>
2024-02-23 21:58:50 +0530

    First Commit

---
+++ ninjachefs/frontend/src/components/navigation/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { UserButton } from "@clerk/clerk-react";
+
+export default function Header() {
+
+    return (
+        <header className="h-28 md:h-24 flex items-center justify-center md:justify-between flex-col md:flex-row">
+
+            <Link to="../"> <img  className="h-11 w-auto" src={logo} alt="Ninja Chefs Logo"/></Link>
+
+            <section>
+                <UserButton></UserButton>
+            </section>
+            
+            
+        </header>
+    );
+  }
